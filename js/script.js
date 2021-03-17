71651a91067afc058e06f21db6efeb9e8e2413fa
Gustavo Benedito <gustavobeneditodeveloper@gmail.com>
2021-03-17 15:59:13 -0300

    add de icons

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+/*
+var icontrash = document.getElementById("icons-trash")
+var iconshare = document.getElementById("icons-share")
+var iconspen = document.getElementById("icons-pen")
+
+
+function entrar(){
+    icontrash.style.display = "block";
+    iconshare.style.display = "block";
+    iconspen.style.display = "block";
+    }
+function sair(){
+    icontrash.style.display = "none";
+    iconshare.style.display = "none";
+    iconspen.style.display = "none";
+
+}
+*/
+
+
+
